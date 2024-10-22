d1d5636cbd5e69b0ec9069d3331417dd473f286e
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-10-23 03:25:07 +0700

    memberbaiki tampilan product page

--- js/product.js
+++ js/product.js
@@ -1,24 +1,106 @@
-const sizeOptions = document.querySelectorAll(".size-option");
-const images = document.querySelectorAll(".images");
+//image slider
+const images = document.querySelectorAll(".grid-img img");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
-sizeOptions.forEach((button) => {
-  button.addEventListener("click", () => {
-    sizeOptions.forEach((btn) => btn.classList.remove("active"));
+let currentIndex = 0;
 
-    button.classList.add("active");
+const updateSliderPosition = () => {
+  console.log(currentIndex);
+  const translateX = -currentIndex * 500;
+  document.querySelector(
+    ".grid-img"
+  ).style.transform = `translateX(${translateX}px)`;
+};
+
+prevBtn.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = images.length - 1;
+  }
+  updateSliderPosition();
+});
+
+nextBtn.addEventListener("click", () => {
+  if (currentIndex < images.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  updateSliderPosition();
+});
+
+//add and substract
+let totalItems = 1;
+
+const price = parseInt(
+  document.getElementById("price").innerHTML.trim().slice(3).replaceAll(".", "")
+);
+
+const add = document.getElementById("add");
+const substract = document.getElementById("substract");
+const subtotal = document.getElementById("sub-total");
+const displayItems = document.getElementById("display-item");
+let totalPrice = displayItems.value;
+
+const updateSubTotal = () => {
+  totalPrice = totalItems * price;
+  displayItems.value = totalItems;
+  subtotal.innerHTML = `Rp ${totalPrice.toLocaleString("id-ID")},00`;
+};
+
+add.addEventListener("click", () => {
+  totalItems = totalItems + 1;
+  updateSubTotal();
+});
+
+substract.addEventListener("click", () => {
+  if (totalItems > 1) {
+    totalItems = totalItems - 1;
+    updateSubTotal();
+  }
+});
+
+displayItems.addEventListener("change", () => {
+  if (displayItems.value >= 1) {
+    totalItems = displayItems.value;
+  } else {
+    totalItems = 1;
+  }
+  updateSubTotal();
+});
+
+//size
+const sizes = document.querySelectorAll(".size-option");
+
+sizes.forEach((size, i) => {
+  size.addEventListener("click", () => {
+    sizes.forEach((size, i) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
   });
 });
 
-images.forEach((image) => {
-  image.addEventListener("mouseenter", () => {
-    const getZoomElement = document.getElementById("zoom");
-    getZoomElement.setAttribute("src", image.src);
-  });
+//size info
+
+document.addEventListener("click", function (event) {
+  const sizeInfo = document.getElementById("size-info");
+
+  if (!sizeInfo.contains(event.target)) {
+    console.log("Clicked outside of Size Information!");
+    sizeInfo.classList.add("move-out");
+  }
 });
 
-images.forEach((image) => {
-  image.addEventListener("mouseleave", () => {
-    const getZoomElement = document.getElementById("zoom");
-    getZoomElement.setAttribute("src", "");
-  });
+document.getElementById("info").addEventListener("click", function () {
+  const sizeInfo = document.getElementById("size-info");
+
+  sizeInfo.style.left = "50%";
+  sizeInfo.style.top = "50%";
+  sizeInfo.style.transform = "translate(-50%, -50%)";
+  sizeInfo.classList.remove("move-out");
 });
